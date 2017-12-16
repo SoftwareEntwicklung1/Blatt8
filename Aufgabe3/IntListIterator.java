548ade3e294059e809486539e39e522438267230
Xuanle <33765295+XuanleLin@users.noreply.github.com>
2017-12-16 13:59:43 +0100

    update

---
+++ Aufgabe3/IntListIterator.java
@@ -0,0 +1,23 @@
+interface IteratorOfInt{
+  boolean hasNext();
+  int next();
+}
+public class IntListIterator implements IteratorOfInt{
+  private Node current;
+
+  public IntListIterator(Node e) {
+    this.current = e;
+  }
+
+  //prueft, ob es noch weitere Eintraege gibt
+  public boolean hasNext() {
+    return current != null;
+  }
+
+  //liefert den naechsten Eintrag
+  public int next() {
+    int res = current.getValue();
+    current = current.getNext();
+    return res;
+  }
+}
